1c68c4a2a7b5f10212bb8ced57fe87a9a5b12ee8
Henry Zheng <henry.zheng727@gmail.com>
2025-12-26 09:02:36 -0700

    add multichar ops support and finish lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,9 +9,11 @@ pub enum Token {
 
     // OPERATORS
     ASSIGN,   // "="
+    EQ,       // "=="
     PLUS,     // "+"
     MINUS,    // "-"
     BANG,     // "!"
+    NOTEQ,    // "!="
     ASTERISK, // "*"
     SLASH,    // "/"
     LT,       // "<"
@@ -61,13 +63,30 @@ pub fn next_token(program: &str) -> (Token, &str) {
     let program = eat_whitespace(program);
 
     // Determine what the first token is and lex it.
-    match program.chars().next() {
+    let mut chars = program.chars();
+    match chars.next() {
         Some(char) => {
             return match char {
-                '=' => (Token::ASSIGN, &program[1..]),
+                '=' => {
+                    if let Some(next_c) = chars.next()
+                        && (next_c == '=')
+                    {
+                        (Token::EQ, &program[2..])
+                    } else {
+                        (Token::ASSIGN, &program[1..])
+                    }
+                }
                 '+' => (Token::PLUS, &program[1..]),
                 '-' => (Token::MINUS, &program[1..]),
-                '!' => (Token::BANG, &program[1..]),
+                '!' => {
+                    if let Some(next_c) = chars.next()
+                        && (next_c == '=')
+                    {
+                        (Token::NOTEQ, &program[2..])
+                    } else {
+                        (Token::BANG, &program[1..])
+                    }
+                }
                 '*' => (Token::ASTERISK, &program[1..]),
                 '/' => (Token::SLASH, &program[1..]),
                 '<' => (Token::LT, &program[1..]),
@@ -339,6 +358,104 @@ mod tests {
             Token::FALSE,
             Token::SEMICOLON,
             Token::RBRACE,
+            Token::EOF,
+        ];
+        assert_eq!(expected, lex(test_string));
+    }
+
+    #[test]
+    fn test_large() {
+        let test_string = r#"
+            let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+                x + y;
+            };
+            let result = add(five, ten);
+            !-/*5;
+            5 < 10 > 5;
+            if (5 < 10) {
+                return true;
+            } else {
+                return false;
+            }
+            10 == 10;
+            10 != 9;
+        "#;
+        let expected = vec![
+            Token::LET,
+            Token::IDENT(String::from("five")),
+            Token::ASSIGN,
+            Token::INT(5),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("ten")),
+            Token::ASSIGN,
+            Token::INT(10),
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("add")),
+            Token::ASSIGN,
+            Token::FUNCTION,
+            Token::LPAREN,
+            Token::IDENT(String::from("x")),
+            Token::COMMA,
+            Token::IDENT(String::from("y")),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::IDENT(String::from("x")),
+            Token::PLUS,
+            Token::IDENT(String::from("y")),
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::SEMICOLON,
+            Token::LET,
+            Token::IDENT(String::from("result")),
+            Token::ASSIGN,
+            Token::IDENT(String::from("add")),
+            Token::LPAREN,
+            Token::IDENT(String::from("five")),
+            Token::COMMA,
+            Token::IDENT(String::from("ten")),
+            Token::RPAREN,
+            Token::SEMICOLON,
+            Token::BANG,
+            Token::MINUS,
+            Token::SLASH,
+            Token::ASTERISK,
+            Token::INT(5),
+            Token::SEMICOLON,
+            Token::INT(5),
+            Token::LT,
+            Token::INT(10),
+            Token::GT,
+            Token::INT(5),
+            Token::SEMICOLON,
+            Token::IF,
+            Token::LPAREN,
+            Token::INT(5),
+            Token::LT,
+            Token::INT(10),
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::RETURN,
+            Token::TRUE,
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::ELSE,
+            Token::LBRACE,
+            Token::RETURN,
+            Token::FALSE,
+            Token::SEMICOLON,
+            Token::RBRACE,
+            Token::INT(10),
+            Token::EQ,
+            Token::INT(10),
+            Token::SEMICOLON,
+            Token::INT(10),
+            Token::NOTEQ,
+            Token::INT(9),
+            Token::SEMICOLON,
             Token::EOF,
         ];
         assert_eq!(expected, lex(test_string));
