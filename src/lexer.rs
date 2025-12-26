6bc48fb76a663b3e737bcdfaf872838c3c34d10d
Henry Zheng <henry.zheng727@gmail.com>
2025-12-26 04:25:15 -0700

    allow lexer to recognize all single-char tokens

--- src/lexer.rs
+++ src/lexer.rs
@@ -24,8 +24,58 @@ pub enum Token {
     LET,
 }
 
+/// Given the program represented as a string, lex it into tokens.
 pub fn lex(program: &str) -> Vec<Token> {
-    unimplemented!()
+    let mut tokens = Vec::new();
+    let mut curr_string: &str = &program;
+
+    loop {
+        let (next_token, next_string) = next_token(curr_string);
+        match next_token {
+            Token::EOF => {
+                tokens.push(next_token);
+                break;
+            }
+            _ => tokens.push(next_token),
+        }
+        curr_string = next_string;
+    }
+
+    tokens
+}
+
+/// Given the program represented as a string, get the next token from it.
+pub fn next_token(program: &str) -> (Token, &str) {
+    // Cut leading whitespace.
+    let program = eat_whitespace(program);
+
+    // Determine what the first token is and lex it.
+    match program.chars().next() {
+        Some(char) => {
+            return match char {
+                '=' => (Token::ASSIGN, &program[1..]),
+                '+' => (Token::PLUS, &program[1..]),
+                ',' => (Token::COMMA, &program[1..]),
+                ';' => (Token::SEMICOLON, &program[1..]),
+                '(' => (Token::LPAREN, &program[1..]),
+                ')' => (Token::RPAREN, &program[1..]),
+                '{' => (Token::LBRACE, &program[1..]),
+                '}' => (Token::RBRACE, &program[1..]),
+                _ => (Token::ILLEGAL(char), &program[1..]),
+            };
+        }
+        None => return (Token::EOF, program),
+    }
+}
+
+/// Cuts all whitespace from the start of a string.
+pub fn eat_whitespace(string: &str) -> &str {
+    for (index, character) in string.chars().enumerate() {
+        if !character.is_whitespace() {
+            return &string[index..];
+        }
+    }
+    return "";
 }
 
 #[cfg(test)]
@@ -36,6 +86,23 @@ mod tests {
     #[test]
     fn test_small() {
         let test_string = "=+(){},;";
+        let expected = vec![
+            Token::ASSIGN,
+            Token::PLUS,
+            Token::LPAREN,
+            Token::RPAREN,
+            Token::LBRACE,
+            Token::RBRACE,
+            Token::COMMA,
+            Token::SEMICOLON,
+            Token::EOF,
+        ];
+        assert_eq!(expected, lex(test_string));
+    }
+
+    #[test]
+    fn test_small_with_whitespace() {
+        let test_string = "        =+(){},;       ";
         let expected = vec![
             Token::ASSIGN,
             Token::PLUS,
@@ -110,8 +177,15 @@ mod tests {
     }
 
     #[test]
+    fn test_illegal_with_whitespace() {
+        let test_string = "  ~   ~    ";
+        let expected = vec![Token::ILLEGAL('~'), Token::ILLEGAL('~'), Token::EOF];
+        assert_eq!(expected, lex(test_string));
+    }
+
+    #[test]
     fn test_empty_string() {
-        let test_string = "=+(){},;";
+        let test_string = "";
         let expected = vec![Token::EOF];
         assert_eq!(expected, lex(test_string));
     }
