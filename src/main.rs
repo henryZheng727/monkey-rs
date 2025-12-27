5531eaacd007ecec96633958cd3b149a04c68766
Henry Zheng <henry.zheng727@gmail.com>
2025-12-26 20:29:46 -0700

    start a basic repl that lexes and prints tokens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 mod lexer;
 
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // display the prompt
+        print!(">> ");
+        let _ = std::io::stdout().flush();
+
+        // read input
+        let mut input = String::new();
+        let read_bytes = std::io::stdin()
+            .read_line(&mut input)
+            .expect("Cannot open interactive terminal.");
+
+        // close REPL on EOF
+        if read_bytes <= 0 {
+            break;
+        }
+
+        // lex input
+        let tokens = lexer::lex(&input);
+
+        // print results
+        println!("{:?}", tokens);
+    }
 }
