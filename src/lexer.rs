c2e01c54fc5e3a1a090298764956f307cee060db
Henry Zheng <henry.zheng727@gmail.com>
2025-12-27 03:06:18 -0700

    clean up lexer logic and add tests

--- src/lexer.rs
+++ src/lexer.rs
@@ -58,65 +58,68 @@ pub fn lex(program: &str) -> Vec<Token> {
 }
 
 /// Given the program represented as a string, get the next token from it.
-pub fn next_token(program: &str) -> (Token, &str) {
-    // Cut leading whitespace.
+fn next_token(program: &str) -> (Token, &str) {
+    // cut leading whitespace
     let program = eat_whitespace(program);
 
-    // Determine what the first token is and lex it.
+    // determine what the first token is and lex it
     let mut chars = program.chars();
-    match chars.next() {
-        Some(char) => {
-            return match char {
-                '=' => {
-                    if let Some(next_c) = chars.next()
-                        && (next_c == '=')
-                    {
-                        (Token::EQ, &program[2..])
-                    } else {
-                        (Token::ASSIGN, &program[1..])
-                    }
+    return match chars.next() {
+        Some(char) => match char {
+            '=' => {
+                if let Some(next_c) = chars.next()
+                    && (next_c == '=')
+                {
+                    (Token::EQ, &program[2..])
+                } else {
+                    (Token::ASSIGN, &program[1..])
                 }
-                '+' => (Token::PLUS, &program[1..]),
-                '-' => (Token::MINUS, &program[1..]),
-                '!' => {
-                    if let Some(next_c) = chars.next()
-                        && (next_c == '=')
-                    {
-                        (Token::NOTEQ, &program[2..])
-                    } else {
-                        (Token::BANG, &program[1..])
-                    }
+            }
+            '+' => (Token::PLUS, &program[1..]),
+            '-' => (Token::MINUS, &program[1..]),
+            '!' => {
+                if let Some(next_c) = chars.next()
+                    && (next_c == '=')
+                {
+                    (Token::NOTEQ, &program[2..])
+                } else {
+                    (Token::BANG, &program[1..])
                 }
-                '*' => (Token::ASTERISK, &program[1..]),
-                '/' => (Token::SLASH, &program[1..]),
-                '<' => (Token::LT, &program[1..]),
-                '>' => (Token::GT, &program[1..]),
-                ',' => (Token::COMMA, &program[1..]),
-                ';' => (Token::SEMICOLON, &program[1..]),
-                '(' => (Token::LPAREN, &program[1..]),
-                ')' => (Token::RPAREN, &program[1..]),
-                '{' => (Token::LBRACE, &program[1..]),
-                '}' => (Token::RBRACE, &program[1..]),
-                'a'..='z' | 'A'..='Z' => read_ident(program),
-                '0'..='9' => read_int(program),
-                _ => (Token::ILLEGAL(char), &program[1..]),
-            };
-        }
-        None => return (Token::EOF, program),
-    }
+            }
+            '*' => (Token::ASTERISK, &program[1..]),
+            '/' => (Token::SLASH, &program[1..]),
+            '<' => (Token::LT, &program[1..]),
+            '>' => (Token::GT, &program[1..]),
+            ',' => (Token::COMMA, &program[1..]),
+            ';' => (Token::SEMICOLON, &program[1..]),
+            '(' => (Token::LPAREN, &program[1..]),
+            ')' => (Token::RPAREN, &program[1..]),
+            '{' => (Token::LBRACE, &program[1..]),
+            '}' => (Token::RBRACE, &program[1..]),
+            'a'..='z' | 'A'..='Z' => read_ident(program),
+            '0'..='9' => read_int(program),
+            _ => (Token::ILLEGAL(char), &program[1..]),
+        },
+        None => (Token::EOF, program),
+    };
 }
 
 /// Cuts all whitespace from the start of a string.
-pub fn eat_whitespace(string: &str) -> &str {
-    for (index, character) in string.chars().enumerate() {
-        if !character.is_whitespace() {
-            return &string[index..];
+fn eat_whitespace(string: &str) -> &str {
+    // collect all whitespace at start of string
+    let mut first_non_whitespace = string.len();
+    for (index, char) in string.chars().enumerate() {
+        if !char.is_whitespace() {
+            first_non_whitespace = index;
+            break;
         }
     }
-    return "";
+
+    // slice the leading whitespace
+    return &string[first_non_whitespace..];
 }
 
-pub fn read_ident(string: &str) -> (Token, &str) {
+fn read_ident(string: &str) -> (Token, &str) {
     // an identifier is [a-zA-Z_]+
     fn is_letter(c: char) -> bool {
         c.is_alphabetic() || c == '_'
@@ -136,31 +139,36 @@ pub fn read_ident(string: &str) -> (Token, &str) {
         }
     }
 
-    for (index, character) in string.chars().enumerate() {
-        if !is_letter(character) {
-            return (lookup_ident(&string[..index]), &string[index..]);
+    // collect all letters at start of string
+    let mut first_non_whitespace = string.len();
+    for (index, char) in string.chars().enumerate() {
+        if !is_letter(char) {
+            first_non_whitespace = index;
+            break;
         }
     }
 
-    return (lookup_ident(string), "");
+    // construct a token
+    let ident = &string[..first_non_whitespace];
+    let rest = &string[first_non_whitespace..];
+    return (lookup_ident(ident), rest);
 }
 
-pub fn read_int(string: &str) -> (Token, &str) {
-    for (index, character) in string.chars().enumerate() {
-        if !character.is_numeric() {
-            let val = match usize::from_str_radix(&string[..index], 10) {
-                Ok(value) => value,
-                Err(_) => unreachable!(),
-            };
-            return (Token::INT(val), &string[index..]);
+fn read_int(string: &str) -> (Token, &str) {
+    // collect all integer characters at start of string
+    let mut first_non_int = string.len();
+    for (index, char) in string.chars().enumerate() {
+        if !char.is_numeric() {
+            first_non_int = index;
+            break;
         }
     }
 
-    let val = match usize::from_str_radix(&string, 10) {
-        Ok(value) => value,
-        Err(_) => unreachable!(),
-    };
-    return (Token::INT(val), "");
+    // construct a token
+    let int = &string[..first_non_int];
+    let rest = &string[first_non_int..];
+    let val = usize::from_str_radix(int, 10).unwrap();
+    return (Token::INT(val), rest);
 }
 
 #[cfg(test)]
@@ -169,7 +177,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_small() {
+    fn test_lex_small() {
         let test_string = "=+(){},;";
         let expected = vec![
             Token::ASSIGN,
@@ -186,7 +194,7 @@ mod tests {
     }
 
     #[test]
-    fn test_small_with_whitespace() {
+    fn test_lex_small_with_whitespace() {
         let test_string = "        =+(){},;       ";
         let expected = vec![
             Token::ASSIGN,
@@ -203,7 +211,7 @@ mod tests {
     }
 
     #[test]
-    fn test_medium() {
+    fn test_lex_medium() {
         let test_string = r#"
             let five = 5;
             let ten = 10;
@@ -255,28 +263,28 @@ mod tests {
     }
 
     #[test]
-    fn test_illegal_small() {
+    fn test_lex_illegal_small() {
         let test_string = "~";
         let expected = vec![Token::ILLEGAL('~'), Token::EOF];
         assert_eq!(expected, lex(test_string));
     }
 
     #[test]
-    fn test_illegal_with_whitespace() {
+    fn test_lex_illegal_with_whitespace() {
         let test_string = "  ~   ~    ";
         let expected = vec![Token::ILLEGAL('~'), Token::ILLEGAL('~'), Token::EOF];
         assert_eq!(expected, lex(test_string));
     }
 
     #[test]
-    fn test_empty_string() {
+    fn test_lex_empty_string() {
         let test_string = "";
         let expected = vec![Token::EOF];
         assert_eq!(expected, lex(test_string));
     }
 
     #[test]
-    fn test_large_no_multichar_ops() {
+    fn test_lex_large_no_multichar_ops() {
         let test_string = r#"
             let five = 5;
             let ten = 10;
@@ -364,7 +372,7 @@ mod tests {
     }
 
     #[test]
-    fn test_large() {
+    fn test_lex_large() {
         let test_string = r#"
             let five = 5;
             let ten = 10;
@@ -459,5 +467,75 @@ mod tests {
             Token::EOF,
         ];
         assert_eq!(expected, lex(test_string));
+    }
+
+    #[test]
+    fn test_eat_whitespace_small() {
+        let test_string = "     test    ";
+        let expected = "test    ";
+        assert_eq!(expected, eat_whitespace(test_string));
+    }
+
+    #[test]
+    fn test_eat_whitespace_empty_string() {
+        let test_string = "";
+        let expected = "";
+        assert_eq!(expected, eat_whitespace(test_string));
+    }
+
+    #[test]
+    fn test_eat_whitespace_no_whitespace() {
+        let test_string = "nowhitespace";
+        let expected = "nowhitespace";
+        assert_eq!(expected, eat_whitespace(test_string));
+    }
+
+    #[test]
+    fn test_eat_whitespace_newlines() {
+        let test_string = "\n\n\n\n\ntest";
+        let expected = "test";
+        assert_eq!(expected, eat_whitespace(test_string));
+    }
+
+    #[test]
+    fn test_read_ident_small() {
+        let test_string = "test the following";
+        let expected = (Token::IDENT(String::from("test")), " the following");
+        assert_eq!(expected, read_ident(test_string));
+    }
+
+    #[test]
+    fn test_read_ident_one_char() {
+        let test_string = "a b c";
+        let expected = (Token::IDENT(String::from("a")), " b c");
+        assert_eq!(expected, read_ident(test_string));
+    }
+
+    #[test]
+    fn test_read_ident_full_string() {
+        let test_string = "tokenisfullstring";
+        let expected = (Token::IDENT(String::from("tokenisfullstring")), "");
+        assert_eq!(expected, read_ident(test_string));
+    }
+
+    #[test]
+    fn test_read_int_small() {
+        let test_string = "1234 test";
+        let expected = (Token::INT(1234), " test");
+        assert_eq!(expected, read_int(test_string));
+    }
+
+    #[test]
+    fn test_read_int_one_char() {
+        let test_string = "1 2 3";
+        let expected = (Token::INT(1), " 2 3");
+        assert_eq!(expected, read_int(test_string));
+    }
+
+    #[test]
+    fn test_read_int_full_string() {
+        let test_string = "1234";
+        let expected = (Token::INT(1234), "");
+        assert_eq!(expected, read_int(test_string));
     }
 }
